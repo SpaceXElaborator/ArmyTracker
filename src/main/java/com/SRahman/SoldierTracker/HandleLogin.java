91d72546fbc42f9f3597acc7297126907d008e16
Sean Rahman <terturlminecraft@gmail.com>
2021-05-03 13:17:44 -0700

    Handling login and redirection has been added. Session attributes have been implemented but might need a bit more work. web.xml has secure and ssl over http to help with session jacking.

--- src/main/java/com/SRahman/SoldierTracker/HandleLogin.java
+++ src/main/java/com/SRahman/SoldierTracker/HandleLogin.java
@@ -10,6 +10,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 @WebServlet("/HandleLogin")
 public class HandleLogin extends HttpServlet {
@@ -24,27 +25,40 @@ public class HandleLogin extends HttpServlet {
 		String username = request.getParameter("login");
 		String password = request.getParameter("password");
 		
-		response.getWriter().write(username + " | " + password);
-		
-		// Issue with Sqlite db not reading from resource 
-		response.getWriter().write(HandleLogin.class.getClassLoader().getResource("SqliteLTDatabase.db").toString());
 		try {
-			System.out.println(Database.checkLogin(username, password));
+			if(Database.checkLogin(username, password)) {
+				HttpSession sess = request.getSession();
+				sess.setAttribute("user", username);
+				sess.setMaxInactiveInterval(30*60);
+				if(sess.getAttribute("user") != null || sess.getAttribute("user") != "") {
+					// TODO: Send redirect to admin portal
+					response.getWriter().write("Will Send to Tracker webpage");
+					return;
+				} else {
+					response.sendRedirect("index.jsp");
+					return;
+				}
+			} else {
+				response.sendRedirect("index.jsp");
+				return;
+			}
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
+			response.sendRedirect("index.jsp");
+			return;
 		}
 		
-		// TODO: attempt connection to database
-			// Check for username/password
-				// set attributes of loggedin and username (Best approach for security?)
-				// Check if attributes were set
-					// create session and redirect to tracker.jsp (Check for session hijacking?)
+		// TODO: attempt connection to database 													----
+			// Check for username/password 															----
+				// set attributes of loggedin and username (Best approach for security?)			---?
+				// Check if attributes were set														----
+					// create session and redirect to tracker.jsp (Check for session hijacking?)	----
 					// LOOKUP: https://stackoverflow.com/questions/16912492/session-hijacking-prevention-in-java-struts-2-0-error-encountered
 					// LOOKUP: https://www.selikoff.net/2017/10/04/javaone-hacker-guide-session-hijacking/
-				// not set
-					// Send to index.jsp
-			// send to index.jsp if login fails
-		// send to index.jsp if can't connect to database
+				// not set																			----
+					// Send to index.jsp															----
+			// send to index.jsp if login fails														----
+		// send to index.jsp if can't connect to database											----
 	}
 
 }
