ba38f9f925963de0944f2143badc8bba27ad13cd
Sean Rahman <terturlminecraft@gmail.com>
2021-05-03 13:16:40 -0700

    Fixed issue with Sqlite not writing data. Moved to a local file that will be inside the running users home directory under the "LTProject" folder

--- src/main/java/com/SRahman/SoldierTracker/Util/Database.java
+++ src/main/java/com/SRahman/SoldierTracker/Util/Database.java
@@ -18,19 +18,41 @@ import org.apache.commons.codec.binary.Hex;
 
 public class Database {
 	
-	private static void createTables() throws ClassNotFoundException, SQLException {
+	public static void checkSetup() throws ClassNotFoundException, SQLException {
 		
-		File f = new File("SqliteLTDatabase.db");
+		File f = new File(System.getProperty("user.home"), "LTProject");
+		
 		if(!f.exists()) {
+			f.mkdir();
+			File database = new File(f, "SqliteLTDatabase.db");
 			try {
-				f.createNewFile();
+				database.createNewFile();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 			Class.forName("org.sqlite.JDBC");
-			Connection conn = DriverManager.getConnection("jdbc:sqlite:SqliteLTDatabase.db");
+			Connection conn = DriverManager.getConnection("jdbc:sqlite:" + System.getProperty("user.home") + "\\LTProject\\SqliteLTDatabase.db");
 			PreparedStatement stat = conn.prepareStatement("CREATE TABLE IF NOT EXISTS users (id integer PRIMARY KEY, username varchar(255), password longtext);");
 			stat.execute();
+			
+			// Check if the Admin user is present.
+			PreparedStatement checkAdmin = conn.prepareStatement("SELECT * FROM users WHERE username = ?");
+			checkAdmin.setString(1, "admin");
+			ResultSet rs = checkAdmin.executeQuery();
+			if(!rs.next()) {
+				// If there is no admin, add the user with a default password
+				String username = "admin";
+				String password = "";
+				try {
+					password = hashPassword("AdminTestPass");
+				} catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
+					e.printStackTrace();
+				}
+				PreparedStatement addAdmin = conn.prepareStatement("INSERT INTO users (username, password) values (?, ?)");
+				addAdmin.setString(1, username);
+				addAdmin.setString(2, password);
+				addAdmin.executeUpdate();
+			}
 			conn.close();
 		}
 	}
@@ -45,9 +67,9 @@ public class Database {
 	}
 	
 	public static void addUser(String username, String password) throws ClassNotFoundException, SQLException, InvalidKeySpecException, NoSuchAlgorithmException {
-		createTables();
+		checkSetup();
 		Class.forName("org.sqlite.JDBC");
-		Connection conn = DriverManager.getConnection("jdbc:sqlite:SqliteLTDatabase.db");
+		Connection conn = DriverManager.getConnection("jdbc:sqlite:" + System.getProperty("user.home") + "\\LTProject\\SqliteLTDatabase.db");
 		PreparedStatement stat = conn.prepareStatement("INSERT INTO users(username, password) VALUES (?,?)");
 		stat.setString(1, username);
 		stat.setString(2, hashPassword(password));
@@ -56,9 +78,9 @@ public class Database {
 	}
 	
 	public static boolean checkLogin(String username, String password) throws ClassNotFoundException, SQLException {
-		createTables();
+		checkSetup();
 		Class.forName("org.sqlite.JDBC");
-		Connection conn = DriverManager.getConnection("jdbc:sqlite:SqliteLTDatabase.db");
+		Connection conn = DriverManager.getConnection("jdbc:sqlite:" + System.getProperty("user.home") + "\\LTProject\\SqliteLTDatabase.db");
 		PreparedStatement stat = conn.prepareStatement("SELECT * FROM users WHERE username = ?");
 		stat.setString(1, username);
 		ResultSet result = stat.executeQuery();
