2978b07b329247fd18923bebfbe6a2200b08cb00
Sean Rahman <terturlminecraft@gmail.com>
2021-05-13 19:52:56 -0700

    0.0.4 - Added the basic of being able to start looking into handling web POST requests and moving it into a SQLite database. Added the Flask Secret Key to start working on session handling to be saved for logging in and logging out functionality.

--- ArmyTracker/Tracker.py
+++ ArmyTracker/Tracker.py
@@ -4,14 +4,24 @@ Created on May 13, 2021
 @author: Sean Rahman
 '''
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# Random string used to create HTTP sessions using Flask
+app.secret_key = 'urasE3AhMEbFNZfXxRsW'
+
 @app.route('/')
 def index():
-    title = "Test Template File"
-    return render_template('index.html', title=title)
+    return render_template('index.html')
+
+# Take the incoming post request and start working on handling the log-in portion
+@app.route('/HandleLogin', methods = ['POST'])
+def login():
+    if request.method == 'POST':
+        user = request.form['login']
+        test = '<html><body>' + user + '</body></html>'
+        return test
 
 if __name__ == '__main__':
     app.run()
